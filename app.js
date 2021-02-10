cd5c165f75f3c0fc560d2d4420c01ee970bbe853
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-02-10 12:04:42 -0800

    state update

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ Event-Listener
 */
 
 todoBtn.addEventListener('click',todoSubmit );
-todoList.addEventListener('click', deleteTodo)
 
 
 /*
@@ -29,6 +28,25 @@ function todoSubmit(e){
 
 
     //creating dynamic list
-   
+   const todoDiv = document.createElement('div');
+   todoDiv.classList.add('todo');
+   //
+   const newTodo = document.createElement('li');
+   newTodo.classList.add('todo-item');
+   newTodo.innerText = todoInput.value;
+   //
+   todoDiv.appendChild(newTodo)
+   //
+   const completeBtn = document.createElement('button');
+   completeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+   completeBtn.classList.add('complete-btn')
+   todoDiv.appendChild(completeBtn);
+   //
+   const trashedBtn = document.createElement('button');
+   trashedBtn.classList.add('trash-btn');
+   trashedBtn.innerHTML = '<i class="fas fa-check"></i>';
+   todoDiv.appendChild(trashedBtn);
+   //
+   todoList.appendChild(todoDiv)
     todoInput.value = ""
 };
